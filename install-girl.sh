5c4b3c8aa9d6fd159bc9fbdd8a239a105ae07387
CaptainZalo <captzalo@gmail.com>
2016-08-20 15:57:10 +0200

    Update install-girl.sh
    Cleaned up script.

--- install-girl.sh
+++ install-girl.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
+# Installer script v0.1. Intended for Debian ARM-based SoC-distribution and GIRL v9.5.0. http://girl.software
+# Check if Synaptic packagemanager is installed.
 set -e
 if ! hash apt-get 2>/dev/null; then
-        echo "This script requires a Debian based distribution."
+        echo "Sorry. This script requires a distribution with Synaptic package manager (Debian distributions)."
         exit 1
 fi
-
+# Check if script is ran in root context.
 if [ "$(id -u)" -ne "0" ]; then
         echo "This script requires root."
         exit 1
 fi
-# update repositories
+# Update repositories.
 apt-get -y update
-# install build basics.
-# testing content of build-essentials. Remarrked line below is complete list of speecific dependencies. Note: didn't work.
-apt-get -y install pkg-config gcc gtk+-2.0 glib-2.0 libgnome-2.0 libgnomeui-dev intltool itstool streamripper audacious
-# apt-get -y install build-essentials streamripper audacious
-# get GIRL source, unzip and configure
+# Install build dependencies.
+apt-get -y install pkg-config gcc gtk+-2.0 glib-2.0 libgnome-2.0 libgnomeui-dev intltool itstool streamripper
+# Get GIRL 9.5.0 source tarball, unzip and configure.
 cd /tmp
 wget https://download.gnome.org/sources/girl/9.5/girl-9.5.0.tar.xz
 tar Jxvf girl-9.5.0.tar.xz
 cd girl-9.5.0
 ./configure
-# compile wih Audacious as preferred player. Replace Audacious with preferred audio-player. Use make without switches for Totem as default player.
-make -e GIRL_HELPER_PLAYER=/usr/bin/audacious
+# Compile with Totem as default player.
+make 
+# Compile wih preferred player. Replace <program name> with preferred audio-player. Personal preferences.
+# make -e GIRL_HELPER_PLAYER=/usr/bin/<program name>
 make install
