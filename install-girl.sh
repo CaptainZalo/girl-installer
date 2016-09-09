7c8c885530111f085ee735e1b80318dd39da94e7
CaptainZalo <captzalo@gmail.com>
2016-09-09 20:50:54 +0200

    Update install-girl.sh

--- install-girl.sh
+++ install-girl.sh
@@ -17,9 +17,9 @@ apt-get -y update
 apt-get -y install pkg-config gcc gtk+-2.0 glib-2.0 libgnome-2.0 libgnomeui-dev intltool itstool streamripper
 # Get GIRL 9.5.0 source tarball, unzip and configure.
 cd /tmp
-wget http://current.girl.software/
+wget http://current.girl.software/ -O girl.tar.xz
 tar Jxvf girl*
-cd girl*
+cd girl-*
 ./configure
 # Compile with Totem as default player.
 make 
